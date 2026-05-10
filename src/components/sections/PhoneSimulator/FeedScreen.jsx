40345b0289033b4f9dac680e0e7bc872918af9ed
Arun kumar <itsarunkumarr@users.noreply.github.com>
2026-05-10 14:40:15 +0530

    remove extra demo reel simulator

--- src/components/sections/PhoneSimulator/FeedScreen.jsx
+++ src/components/sections/PhoneSimulator/FeedScreen.jsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { Sparkles, ChevronLeft, Check, ChevronDown, ChevronUp, Music, Play, Volume1, Volume2, Circle, CircleDot, Activity, Search, Bold, Italic, Link, AtSign, Hash, Home, PlusSquare, MessageCircle, User, Heart, Share2, VolumeX, X, Send, Clock, Bell, Plus, Ghost, Lock, Inbox, Wifi, Battery, Edit, ChevronRight, MoreHorizontal, ArrowRight, BellOff, Trash } from 'lucide-react';
 import { vibeData, vibeCategories, musicData, musicCategories, moods, moodStyles, audiences, exploreRecentItems, exploreTrendingData, mockResultsCity, mockResultsRiya, exploreGridItems, mockConversationsData, moodColors } from './MockData';
 
-import { initialHeroReels } from '../Hero/Hero';
+import { initialHeroReels } from './MockData';
 import userAvatar from '../../../assets/avatar.png';
 
 const FeedScreen = () => {
